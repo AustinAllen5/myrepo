84bed874abac50ae02ba04a3e215e1dc6f2d50b8
Austin Allen <austin.allen@duke.edu>
2018-12-03 14:22:56 -0500

    Added run all script

--- src/runAll.R
+++ src/runAll.R
@@ -1,1 +1,5 @@
 #Run all script for BRS example
+source('~/Dropbox/Git/Workshop/myrepo/src/readData.R')
+source('~/Dropbox/Git/Workshop/myrepo/src/wrangle.R')
+source('~/Dropbox/Git/Workshop/myrepo/src/plot.R')
+source('~/Dropbox/Git/Workshop/myrepo/src/stat_analysis.R')
